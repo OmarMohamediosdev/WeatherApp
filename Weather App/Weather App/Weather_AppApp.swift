b660c13c852d1622d58fd9504b4d772c60504390
OmarMohamediosdev <45975191+OmarMohamediosdev@users.noreply.github.com>
2025-10-25 15:15:58 +0300

    initial commit

---
+++ Weather App/Weather App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather App
+//
+//  Created by Omar Mohamed on 25/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
